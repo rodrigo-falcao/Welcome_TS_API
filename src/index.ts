a9e18409d0bf095ebf38238c1c1f3c572ad80aed
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-06-27 16:50:27 -0300

    Feat: Created method Post

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-const projetoBackEnd: {
-    professor: string,
-    escola: string
-} = {
-    professor: "Fernando Barros",
-    escola: "DNC"
-}
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import PostRequest from "./dtos/post.request";
+import PostHandler from "./utils/post.handler";
 
-console.log(projetoBackEnd)
+dotenv.config();
+
+const app: Express = express()
+app.use( bodyParser.json() );
+const port = process.env.PORT || 5000;
+
+app.post('/',(req: Request, res: Response) => {
+    const body: PostRequest = req.body;
+    const response = PostHandler.buildResponse(body);
+    res.json(response)
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
